5f948ad82eb47fa3535437971bccef52a1c2f28d
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-04-17 15:22:38 +0200

    made server robust + rewrote sploit

--- private/day3/server.c
+++ private/day3/server.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include<dlfcn.h>
-#include<stdio.h>
 #include<unistd.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<sys/wait.h>
+#include<signal.h>
 
 const char WELCOME[] = "Your output to my input? Do your best!\n";
 const char REPLY[]   = "Is this your best?:\n";
@@ -51,9 +55,57 @@ void handle_client (int sock) {
   close(sock);
 }
 
+#define BACKLOG 100
+
+void sigchld_handler(int s) {
+  while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+int open_sock (unsigned short int port) {
+  int sock, nsock, nsocklen = sizeof(struct sockaddr_in), yes = 1;
+  struct sockaddr_in saddr;
+  struct sigaction sa;
+
+  memset(&saddr, 0, sizeof saddr);
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(port);
+  saddr.sin_addr.s_addr = INADDR_ANY;
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    perror("setsockopt");
+    exit(1);
+  }
+
+  if (bind(sock, (struct sockaddr *)&saddr, sizeof saddr) == -1) {
+    close(sock);
+    perror("bind");
+    exit(1);
+  }
+
+  if (listen(sock, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  return sock;
+}
+
 int main (int argc, char **argv) {
   unsigned short int port;
-  struct sockaddr_in saddr, caddr;
+  struct sockaddr_in caddr;
   int sock, nsock, nsocklen;
 
   if (argc < 2) {
@@ -62,17 +114,15 @@ int main (int argc, char **argv) {
   }
   port = atoi(argv[1]);
 
-  saddr.sin_family = AF_INET;
-  saddr.sin_port = htons(port);
-  saddr.sin_addr.s_addr = INADDR_ANY;
-
-  sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  bind(sock, (struct sockaddr *)&saddr, sizeof(saddr));
-  listen(sock, 1);
+  sock = open_sock(port);
 
   nsocklen = sizeof(caddr);
   for (;;) {
-    nsock = accept(sock, (struct sockaddr *)&caddr, &nsocklen);
+    if ((nsock = accept(sock, (struct sockaddr *)&caddr, &nsocklen)) == -1) {
+      perror("accept");
+      continue;
+    }
+
     printf("Got connection from %d.%d.%d.%d\n",
            ((caddr.sin_addr.s_addr      ) & 0xFF),
            ((caddr.sin_addr.s_addr >>  8) & 0xFF),
